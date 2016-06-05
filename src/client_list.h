1cfe32f5c2b01933882fa07a0062c4d74c72dfb8
Zelxy <jose.pereiraprog@gmail.com>
2016-06-05 20:17:12 +0100

    Draw list and small fixes

--- src/client_list.h
+++ src/client_list.h
@@ -9,6 +9,7 @@
 #define NO_MEM 1
 #define OUT_OF_RANK 2
 #define NO_LIST 3
+#define NULL_PTR 4
 #define EMPTY_LIST 5
 #define CLIENT_EXISTS 6
 
@@ -20,25 +21,27 @@ typedef struct {
 	int max_size;
 }lista_elems_t;
 
-lista_elems_t* list_create();
+lista_elems_t* clientlst_create();
 
-int list_destroy(lista_elems_t*);
+int clientlst_destroy(lista_elems_t*);
 
-int list_add(lista_elems_t*, elem);
+int clientlst_add(lista_elems_t*, elem);
 
-int list_set(lista_elems_t*, unsigned, elem, elem*);
+int clientlst_set(lista_elems_t*, unsigned, elem, elem*);
 
-int list_get(lista_elems_t*, unsigned, elem*);
+int clientlst_get(lista_elems_t*, unsigned, elem*);
 
-int list_size(lista_elems_t*, unsigned*);
+int clientlst_size(lista_elems_t*, unsigned*);
 
-int list_is_empty(lista_elems_t*);
+int clientlst_is_empty(lista_elems_t*);
 
-int list_elem_exists(lista_elems_t*, char*);
+int clientlst_elem_exists(lista_elems_t*, char*);
 
-void list_update_active(lista_elems_t*,int,int,int);
+void clientlst_update_active(lista_elems_t*,int,int,int);
 
 void client_visit_store(lista_elems_t*,char*,int,char,int,int,int);
+
+void clientlst_draw(lista_elems_t*);
 
 #endif
 
